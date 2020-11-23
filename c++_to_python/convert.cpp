7fc6e306476b2d34d950b2ffd479b6c5f4dbaf30
kawahara <k_hiro@biglobe.ne.jp>
2020-11-23 13:49:11 +0900

    convert.cpp completed

--- c++_to_python/convert.cpp
+++ c++_to_python/convert.cpp
@@ -10,6 +10,28 @@
 #define WIDTH 800
 #define HEIGHT 600
 namespace np = boost::python::numpy;
+std::string type2str(int type) { 
+    std::string r; 
+
+    uchar depth = type & CV_MAT_DEPTH_MASK; 
+    uchar chans = 1 + (type >> CV_CN_SHIFT); 
+
+    switch (depth) { 
+    case CV_8U: r = "8U"; break; 
+    case CV_8S: r = "8S"; break; 
+    case CV_16U: r = "16U"; break; 
+    case CV_16S: r = "16S"; break; 
+    case CV_32S: r = "32S"; break; 
+    case CV_32F: r = "32F"; break; 
+    case CV_64F: r = "64F"; break; 
+    default:  r = "User"; break; 
+    } 
+
+    r += "C"; 
+    r += (chans+'0'); 
+
+    return r; 
+} 
 
 int main() {
 
@@ -25,11 +47,15 @@ int main() {
     std::string script((std::istreambuf_iterator<char>(ifs)),
                         std::istreambuf_iterator<char>());
     cv::Mat depth;
-    depth=cv::imread("../disp_result.png",1);
+    depth=cv::imread("../disp_result.png",0);
+    cv::imshow("original",depth);
     std::cout<<"depth_type: "<<depth.type()<<std::endl;
+
+    std::string ty = type2str(depth.type()); 
+    printf("Matrix: %s %dx%d \n", ty.c_str(), depth.cols, depth.rows);  
     //100x100行列の準備
     //boost::python::tuple shapeA = boost::python::make_tuple(MAX_X, MAX_X);
-    boost::python::tuple shapeA = boost::python::make_tuple(HEIGHT, WIDTH);
+    boost::python::tuple shapeA = boost::python::make_tuple(HEIGHT,WIDTH);
 
     np::ndarray A = np::zeros(shapeA, np::dtype::get_builtin<unsigned char>());
     int width = depth.cols;
@@ -48,15 +74,17 @@ int main() {
     //mat_numpy.mulの実行
     boost::python::exec(script.c_str(), main_ns);
     auto func = main_ns["mul"];
+
     auto pyresult_numpy = func(A);
 
     //結果の受け取り
     //stl_input_iteratorを使ってタプル全要素を受け取る
     boost::python::stl_input_iterator<np::ndarray> begin(pyresult_numpy), end;
     std::list<np::ndarray> pyresult_list(begin, end);
-    cv::Mat received(600,600,CV_8U);
+    cv::Mat received(600,800,CV_8UC1);
     for(auto itr = pyresult_list.begin(); itr != pyresult_list.end(); ++itr) {
-        double *p = reinterpret_cast<double *>((*itr).get_data());
+        unsigned char *p = reinterpret_cast<unsigned char *>((*itr).get_data());
+        //double *p = reinterpret_cast<double *>((*itr).get_data());
         //ndarrayでは基本的にメモリは連続領域上に保持されるので、
         //各要素には[]演算子を使ってアクセスできる
         for(int i=0;i<HEIGHT;i++){
--- c++_to_python/mat_numpy.py
+++ c++_to_python/mat_numpy.py
@@ -1,8 +1,13 @@
 # nはnumpy.ndarray
+import numpy as np
+import cv2 as cv
 def mul(n):
     print("from python")
     print("received: " + str(n))
     print("end from python")
-
+    #mat_array=cv.fromarray(n)
+    cv.imshow("depth",n)
+    np.save('depth',n)
     # ndarrayのタプルを返す
-    return (2 * n, 3* n)
+ #   return (2 * n, 3* n)
+    return (n,n)
